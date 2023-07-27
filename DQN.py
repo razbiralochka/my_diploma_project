95c5bdda8b891f66ea473a8465b908fcf2d01c8a
razbiralochka <razbiralochka@yandex.ru>
2023-07-28 02:20:47 +0400

    Second commit

--- DQN.py
+++ DQN.py
@@ -1,20 +1,68 @@
-from tensorflow.keras.layers import Dense, Flatten, Conv2D
+import numpy as np
+import tensorflow as tf
+from tensorflow.keras.layers import Dense
 from tensorflow.keras import Model
 
 
-class DQN1(Model):
+loss_object = tf.keras.losses.MeanSquaredError()
+train_loss = tf.keras.metrics.Mean(name='train_loss')
+train_accuracy = tf.keras.metrics.MeanSquaredError()
+optimizer = tf.keras.optimizers.Adam()
+
+
+class DQN(Model):
   def __init__(self):
     super().__init__()
-    self.d1 = Dense(5, activation='linear')
-    self.d2 = Dense(128, activation='relu')
-    self.d3 = Dense(3)
+    self.d1 = Dense(5, activation='linear', input_shape=(5,))
+    self.d2 = Dense(15, activation='relu')
+    self.d3 = Dense(20, activation='relu')
+    self.d4 = Dense(10, activation='relu')
+    self.d5 = Dense(3)
 
   def call(self, x):
     x = self.d1(x)
     x = self.d2(x)
     x = self.d3(x)
+    x = self.d4(x)
+    x = self.d5(x)
     return x
 
 class Agent():
     def __init__(self):
-        networ = DQN1
+        self.HAL9000 = DQN()
+        self.replay_memoty = list()
+    @tf.function
+    def select_action(self,state):
+        predictions = self.HAL9000(state, training=False)
+        res = tf.argmax(predictions)
+        return predictions
+
+    def add_to_replay(self, arg):
+        self.replay_memoty.append(arg)
+
+    @tf.function
+    def train_step(self,state, target):
+        with tf.GradientTape() as tape:
+            predictions = self.HAL9000(state, training=True)
+            loss = loss_object(target, predictions)
+        gradients = tape.gradient(loss, self.HAL9000.trainable_variables)
+        optimizer.apply_gradients(zip(gradients, self.HAL9000.trainable_variables))
+        train_loss(loss)
+        train_accuracy(target, predictions)
+
+    def train(self):
+        print("Training: ")
+        train_loss.reset_states()
+        train_accuracy.reset_states()
+        for line in self.replay_memoty:
+            state = np.array([line[0]])
+            action = line[1]
+            reward = line[2]
+            next_state = np.array([line[3]])
+            target = reward + 0.99 * np.amax(self.HAL9000(next_state)[0])
+            target_f = self.HAL9000(state).numpy()
+            target_f[0][action] = target
+            self.train_step(state, target_f)
+
+
+        self.replay_memoty.clear()
--- main.py
+++ main.py
@@ -1,28 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import math
-import torch
-import torch.nn as nn
-import torch.nn.functional as F
-
-
-class DQN(nn.Module):
-
-    def __init__(self):
-        super().__init__()
-        self.layer1 = nn.Linear(5, 128)
-        self.layer2 = nn.Linear(128, 128)
-        self.layer3 = nn.Linear(128, 3)
-    def forward(self, x):
-        x = F.relu(self.layer1(x))
-        x = F.relu(self.layer2(x))
-        x = self.layer3(x)
-        return x
-
-
-
-
-model = DQN1()
+from DQN import Agent
+import tensorflow as tf
 
 
 class Calcs():
@@ -48,7 +28,7 @@ class Calcs():
     def get_time(self):
         return self.time_list
 
-    def equs(self):
+    def fly(self):
         r = 1
         phi = 0
         beta = 0
@@ -66,18 +46,22 @@ class Calcs():
         self.time_list.clear()
         T=0
         acc = 0.00351598
-        net = DQN()
-        while T < 250:
+        agent = Agent()
 
+        dt = np.pi / 1000
+        while T < 250:
+            phi = phi % (2*np.pi)
             self.r_list.append(r)#*8371)
             self.time_list.append(v)#*6900.5)
             self.p_list.append(math.degrees(phi))
-            self.l_list.append(math.degrees(u))
+            self.l_list.append(math.degrees(lam))
             self.inc_list.append(math.degrees(inc))
 
-            dt = np.pi/1000
-            inp = torch.tensor([r,inc,omega,lam,beta])
-            u = torch.argmax(net(inp.float())).numpy()
+
+
+            inp = np.array([[r, inc, phi, lam, beta]])
+
+            u = np.argmax(agent.select_action(inp).numpy())-1
 
 
             dlam = beta
@@ -101,13 +85,76 @@ class Calcs():
         print("omega: ", math.degrees(omega))
         print("Time: ",T)
 
+    def train(self):
+        r = 1
+        phi = 0
+        beta = 0
+        lam = 0
+        u = 0
+        v = 0
+        omega = 0
+        inc = math.radians(self.inc0)
+        inc_k = math.radians(self.inc_k)
+        r_k = self.r_k
+        eps = 0
+        T = 0
+        acc = 0.00351598
+        agent = Agent()
+        err = np.sqrt((inc - inc_k) ** 2 + (r - r_k) ** 2)
+        inp = np.array([[r, inc, omega, lam, beta]])
+        dt = np.pi / 1000
+        N = 0
+        K = 0
+        while err > 0.1:
+            if K == 80000:
+                break
+            phi = phi % (2 * np.pi)
+            prev_err = np.sqrt((inc - inc_k) ** 2 + (r - r_k) ** 2)*10e3
+            inp = np.array([[r, inc, omega, lam, beta]])
+            state = inp[0].tolist()
+
+            action = np.argmax(agent.select_action(inp).numpy())
+            eps = np.random.randint(0,100,1)
+            if eps < 10:
+                action = np.random.randint(0,2,1)[0]
+
+            u = action-1
+
+            dlam = beta
+            dbeta = u * 0.001
+            dp = (1 / (r ** 1.5))
+            dr = 2 * acc * (r ** 1.5) * math.cos(lam)
+            di = acc * math.sin(lam) * (r ** 0.5) * np.cos(phi)
+            dom = acc * math.sin(lam) * (r ** 0.5) * np.sin(phi)
+            dV = acc
+
+            lam += dlam * dt
+            beta += dbeta * dt
+            phi += dp * dt
+            r += dr * dt
+            inc += di * dt
+            omega += dom * dt
+            v += dV * dt
+            T += dt
+            N += 1
+            reward = prev_err -  np.sqrt((inc - inc_k) ** 2 + (r - r_k) ** 2)*10e3
+            next_state = [r, inc, omega, lam, beta]
+
+            if N < 500:
+                agent.add_to_replay([state,action,reward,next_state])
+
+            else:
+                K += N
+                N = 0
+                agent.train()
 
 
 calcs = Calcs()
 
 
 
-calcs.equs()
+calcs.train()
+calcs.fly()
 
 angle = calcs.get_angles()
 time = calcs.get_time()
