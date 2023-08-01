4abb4e5689e2cfc83c111f063cdfc1a740658a11
razbiralochka <razbiralochka@yandex.ru>
2023-08-01 07:07:49 +0400

    Another one commit

--- Agent.py
+++ Agent.py
@@ -1,19 +1,19 @@
 import numpy as np
+import random
 import tensorflow as tf
 from tensorflow.keras.layers import Dense
 from tensorflow.keras import Model
 
-
 loss_object = tf.keras.losses.MeanSquaredError()
 train_loss = tf.keras.metrics.Mean(name='train_loss')
 train_accuracy = tf.keras.metrics.MeanSquaredError()
-optimizer = tf.keras.optimizers.Adam(learning_rate=0.01)
+optimizer = tf.keras.optimizers.Adam(learning_rate=0.001)
 
 
 class DQN(Model):
   def __init__(self):
     super().__init__()
-    self.d1 = Dense(5, activation='linear', input_shape=(5,))
+    self.d1 = Dense(5, activation='linear')
     self.d2 = Dense(360, activation='relu')
     self.d3 = Dense(500, activation='relu')
     self.d4 = Dense(100, activation='linear')
@@ -27,19 +27,32 @@ class DQN(Model):
     x = self.d5(x)
     return x
 
+
+class Memory():
+    def __init__(self):
+        self.replay_memory = list()
+    def memorize(self, state, action, reward, next_state,done):
+        self.replay_memory.append((state, action, reward, next_state))
+
+    def generate_batch(self):
+        if len(self.replay_memory) <= 200:
+            mini_batch = self.replay_memory
+        else:
+            mini_batch = random.sample(self.replay_memory, 200)
+        return mini_batch
+
 class Agent():
     def __init__(self):
         self.HAL9000 = DQN()
-        self.replay_memoty = list()
+        self.memory  = Memory()
     @tf.function
     def select_action(self,state):
-        predictions = self.HAL9000(state, training=False)
-        res = tf.argmax(predictions)
-        return predictions
-
-    def add_to_replay(self, arg):
-        self.replay_memoty.append(arg)
-
+        predictions = self.HAL9000(tf.reshape(state,[1,5]), training=False)
+        res = tf.argmax(predictions[0])
+        res = tf.cast(res,tf.float32)
+        return res
+    def remember(self,state, action, reward, next_state,done):
+        self.memory.memorize(state, action, reward, next_state,done)
     @tf.function
     def train_step(self,state, target):
         with tf.GradientTape() as tape:
@@ -50,17 +63,16 @@ class Agent():
         train_loss(loss)
         train_accuracy(target, predictions)
 
-    def train(self):
-        print("___Training___")
+    def learn(self):
         train_loss.reset_states()
         train_accuracy.reset_states()
-        N = len(self.replay_memoty)
-        for i,line in enumerate(self.replay_memoty):
+        mini_batch=self.generate_batch()
+        N = len(mini_batch)
+        for i,line in enumerate(mini_batch):
             state = np.array([line[0]])
             action = line[1]
             reward = line[2]
             next_state = np.array([line[3]])
-            penalty = line[4]
             if i == N-1:
                 target = reward
             else:
@@ -68,5 +80,5 @@ class Agent():
             target_f = self.HAL9000(state).numpy()
             target_f[0][action] = target
             self.train_step(state, target_f)
-
-        self.replay_memoty.clear()
+    def clear_memory(self):
+        self.replay_memory.clear()
---
+++ Enviroment.py
@@ -0,0 +1,47 @@
+import tensorflow as tf
+import math
+
+
+class Enviroment():
+    def __init__(self):
+        self.end_point = tf.constant([46, math.radians(28.6)])
+        radius = 1
+        inclination = math.radians(51)
+        azimuth = 0
+        psi = 0
+        beta = 0
+        self.state =tf.Variable([azimuth, radius, inclination, psi, beta])
+        self.acc = tf.constant(0.00351598)
+        self.gain = tf.constant(0.0024262)
+        self.time_step = tf.constant(0.1)
+        self.Time = tf.Variable(0, dtype=float)
+    def reset(self):
+        state = self.state
+        return state, False
+
+    @tf.function
+    def step(self, action):
+        h = self.time_step
+        done = False
+        az, radius, inclination, psi, beta = tf.unstack(self.state)
+
+        err_1 = tf.norm(self.state[1:3]-self.end_point)
+
+        dpsi = beta
+        dbeta = tf.add(action, -1) * self.gain
+        daz = (1 / (radius ** 1.5))
+        dr = 2 * self.acc * (radius ** 1.5) * tf.cos(psi)
+        di = self.acc * tf.sin(psi) * (radius ** 0.5) * tf.cos(az)
+
+        self.dstate = tf.stack([daz, dr, di, dpsi, dbeta])
+
+        self.state.assign_add(self.dstate*h)
+        err_2 = tf.norm(self.state[1:3] - self.end_point)
+
+        reward = err_1 - err_2
+        self.Time.assign_add(h)
+
+        if self.Time > 250:
+            done = True
+
+        return self.state, reward, done, self.Time
